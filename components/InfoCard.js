2cc5bab20387c4e328c9da6d44212b3a698ae49e
JC Smiley <jcsmileyjr@gmail.com>
2019-03-03 20:48:58 -0600

    Enlarged the card to fill inthe lower half space

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -3,15 +3,27 @@ import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function InfoCard(props){
   return(
-    <View style={styles.infoCardContainer}>
-      <Text><Text style={styles.titleColor}>"Price:"</Text> {props.price}</Text>     
-      <Text><Text style={styles.titleColor}>"Square Feet:"</Text> {props.squareFeet}</Text>
-      <Text><Text style={styles.titleColor}>"Number of beds:"</Text> {props.beds}</Text>
-      <Text><Text style={styles.titleColor}>"Number of baths:"</Text> {props.baths}</Text>
+    <View style={styles.infoCardContainer}>   
+      <View style={styles.details}>
+        <Text style={styles.titleColor}>"Price:"</Text>
+        <Text style={styles.textOnRightSide}> {props.price}</Text>
+      </View>
+      <View style={styles.details}>
+        <Text style={styles.titleColor}>"Square Feet:"</Text>
+        <Text style={styles.textOnRightSide}> {props.squareFeet}</Text>
+      </View>
+      <View style={styles.details}>
+        <Text style={styles.titleColor}>"Number of beds:"</Text>
+        <Text style={styles.textOnRightSide}> {props.beds}</Text>
+      </View>
+      <View style={styles.details}>
+        <Text style={styles.titleColor}>"Number of baths:"</Text>
+        <Text style={styles.textOnRightSide}> {props.baths}</Text>
+      </View>
       <View style={styles.appointmentButtonStyle}>
-      <Button
-        onPress = {props.openAppointmentBook}
-        title="Set an Appointment"/>    
+        <Button
+          onPress = {props.openAppointmentBook}
+          title="Set an Appointment"/>    
       </View>  
     </View>
   );
@@ -19,15 +31,26 @@ export default function InfoCard(props){
 
 const styles = StyleSheet.create({
   infoCardContainer:{
-    //flex: 1,
-    backgroundColor: "pink",
+    flex: 1,
+    alignSelf: "stretch",
+    marginTop: 20,
   },
-
   titleColor: {
     color: "blue",
+    textAlign: "left",
+    fontSize: 16,
   },
-
   appointmentButtonStyle: {
     marginTop: 5,
+  },
+  details:{
+    flexDirection: "row",
+
+  },
+  textOnRightSide:{
+    flex: 1,
+    alignSelf:"stretch",
+    textAlign:"right",
+        fontSize: 18,
   }
 });
